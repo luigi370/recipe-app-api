ce39a5568fe2d9bd756012cd8447e1c6215c465a
Luis Ernesto Assandri <luigi.assandri@icloud.com>
2020-04-24 10:20:46 +0200

    setting django and docker into pycharm

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -23,3 +23,15 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that user edit pages work"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
