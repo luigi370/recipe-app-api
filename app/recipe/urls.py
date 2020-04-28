8b1a6291aa0c1e29e81fb536e6afaf24d4758663
Luis Ernesto Assandri <luigi.assandri@icloud.com>
2020-04-28 08:50:28 +0200

    added recipes

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -5,6 +5,7 @@ from recipe import views
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientsViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 app_name = 'recipe'
 urlpatterns = [
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 from . import serializer
 
 
@@ -32,3 +32,18 @@ class IngredientsViewSet(BaseRecipeAttrViewSet):
     """Manage ingredients in the database"""
     queryset = Ingredient.objects.all()
     serializer_class = serializer.IngredientSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipes in the database"""
+    serializer_class = serializer.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+
+
