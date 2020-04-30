a936938af9ca14a22ec0b587fa80e23744afefdc
Luis Ernesto Assandri <luigi.assandri@icloud.com>
2020-04-30 17:41:06 +0200

    added image support

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from core.models import Tag, Ingredient, Recipe
 
 
@@ -44,3 +45,12 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serialize a recipe detail"""
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_fields = ('id',)
